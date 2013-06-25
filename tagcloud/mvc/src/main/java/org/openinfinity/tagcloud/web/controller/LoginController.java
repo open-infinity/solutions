ad9d4f9c331eb716c9c1a509daa3ebed85686eaf
Kavan Soleimanbeigi <solxiom@gmail.com>
2013-06-25 13:43:09 +0300

    facebook app created and tested with controllers, test domain solxiom.info connected with facebook app -kavan

---
+++ tagcloud/mvc/src/main/java/org/openinfinity/tagcloud/web/controller/LoginController.java
@@ -0,0 +1,76 @@
+package org.openinfinity.tagcloud.web.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.GrantType;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+
+/*
+ * @author: Kavan Soleimanbeigi
+ */
+@Controller
+public class LoginController {
+
+	    private final String client_id = "1376730172543858";
+	    private final String client_secret = "6677a69b0b098cff417ed826ef1f63ca";
+	    private final String app_namespace = "tieto-tagcloud";
+	    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	
+	    @RequestMapping(value = "/test-auth", method = RequestMethod.GET)
+	    public @ResponseBody String authTest(HttpServletResponse response) {
+	        String authurl = createAuthUrl(), error = "";
+
+	        try {
+	            response.sendRedirect(authurl);
+	        } catch (Exception e) {
+	            LOGGER.error(e.toString());
+	            error = e.toString();
+	        }
+	        return "sorry! " + error ;
+	    }    
+	    @RequestMapping(value = "/auth-response", method = RequestMethod.GET)
+	    public @ResponseBody
+	    List<String> auth_response(WebRequest req){
+	    	List<String> list = new LinkedList<String>();
+	    	if(req.getParameter("error") == null){
+	    		String result = "all ok!", token = req.getParameter("code");		    	
+		    	list.add(result);
+		    	list.add(token);
+	    	}else{
+	    		list.add("error: " + req.getParameter("error") );
+	    		list.add("error_code: " + req.getParameter("error_code") );
+	    		list.add("error_description: " + req.getParameter("error_description") );
+	    		list.add("error_reason: " + req.getParameter("error_reason") );
+	    		
+	    		
+	    	}
+	    	
+	        return list;
+	    }
+	
+	    
+	    private String createAuthUrl(){
+	        FacebookConnectionFactory connectionFactory =
+	                new FacebookConnectionFactory(client_id, client_secret);
+
+	        OAuth2Operations oauthOperations = connectionFactory.getOAuthOperations();
+	        OAuth2Parameters params = new OAuth2Parameters();
+	        params.setRedirectUri("http://www.solxiom.info/tagcloud/auth-response");
+	        String authorizeUrl = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, params);                            
+
+	        return authorizeUrl;
+	    }
+}
+
