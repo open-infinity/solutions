78e0360777f6eeab5585ec1784cece35ba312355
jeretoivanen <korisvalmentaja@gmail.com>
2013-10-14 16:42:14 +0300

    Target column improvements.

--- tagcloud/mvc/src/main/webapp/resources/scripts/searchNearbyTargets.js
+++ tagcloud/mvc/src/main/webapp/resources/scripts/searchNearbyTargets.js
@@ -11,6 +11,7 @@ function searchNearbyTargets(lat, lng) {
 		var logged_in = isUserLoggedIn();
 		$.each(resultJson.results, function(index, value) {
 			$list.append(createDiv(value, index, logged_in));
+			resizeRightColumn();
 			var loc = new google.maps.LatLng(value.target.location[1],
 					value.target.location[0]);
 
--- tagcloud/mvc/src/main/webapp/resources/scripts/tagpicker.js
+++ tagcloud/mvc/src/main/webapp/resources/scripts/tagpicker.js
@@ -2,36 +2,43 @@ function initTagSearchOnDocumentReady() {
 	initTokenInput("#required", "#searchModel");
 	initTokenInput("#preferred", "#searchModel");
 	initTokenInput("#nearby", "#searchModel");
-	
-	$("#searchModel").submit(function() {
-		var request = $(this).serializeObject();
-		request.required = $("#required").tokenInput("get");
-		request.preferred = $("#preferred").tokenInput("get");
-		request.nearby = $("#nearby").tokenInput("get");
-		var center = getCenter();
-		request.location = new Array(center.lng(), getMap().getCenter().lat());
-		var bounds = getBounds();
-		request.bounds = new Array(bounds.getNorthEast().lng(), bounds.getNorthEast().lat(), 
-			bounds.getSouthWest().lng(), bounds.getSouthWest().lat());
-		$.postJSON("/tagcloud/", request, function(resultJson){
-			var $list = $("#targetlist");
-			$list.empty();
-			clearMarkers();
-			var logged_in = isUserLoggedIn();
-			$.each(resultJson.results, function(index, value) {
-				  $list.append(createDiv(value, index, logged_in));
-				  var loc = new google.maps.LatLng(value.target.location[1], value.target.location[0]);
-				  
-				  placeNewMarkerWithIndex(value, loc, index, value.target.id);
-			});
-			
-			styleTargetDivs();
 
-		}, function(error){
-			console.log("error: "+error.responseText);
-		});
-		return false;
-	});
+	$("#searchModel").submit(
+			function() {
+				var request = $(this).serializeObject();
+				request.required = $("#required").tokenInput("get");
+				request.preferred = $("#preferred").tokenInput("get");
+				request.nearby = $("#nearby").tokenInput("get");
+				var center = getCenter();
+				request.location = new Array(center.lng(), getMap().getCenter()
+						.lat());
+				var bounds = getBounds();
+				request.bounds = new Array(bounds.getNorthEast().lng(), bounds
+						.getNorthEast().lat(), bounds.getSouthWest().lng(),
+						bounds.getSouthWest().lat());
+				$.postJSON("/tagcloud/", request, function(resultJson) {
+					var $list = $("#targetlist");
+					$list.empty();
+					clearMarkers();
+					var logged_in = isUserLoggedIn();
+					$.each(resultJson.results, function(index, value) {
+						$list.append(createDiv(value, index, logged_in));
+						resizeRightColumn();
+						var loc = new google.maps.LatLng(
+								value.target.location[1],
+								value.target.location[0]);
+
+						placeNewMarkerWithIndex(value, loc, index,
+								value.target.id);
+					});
+
+					styleTargetDivs();
+
+				}, function(error) {
+					console.log("error: " + error.responseText);
+				});
+				return false;
+			});
 }
 
 function initTagAddOnDocumentReady() {
@@ -41,45 +48,42 @@ function initTagAddOnDocumentReady() {
 		theme : "facebook",
 		resultsLimit : 8,
 		tokenLimit : 1,
-        onAdd: function(){
-            $("#tagAddForm").submit();
-            $("#text").tokenInput("clear");
-        }
+		onAdd : function() {
+			$("#tagAddForm").submit();
+			$("#text").tokenInput("clear");
+		}
 	});
-	
-	$("#tagAddForm").submit(function() {
-		console.log("submittaa");
-		var request = $(this).serializeObject();
-		request.text = $("#text").tokenInput("get")[0].text
-		request.id = "not yet generated";
-		var action = document.getElementById("tagAddForm").getAttribute("action");
-		$.postJSON(action, request, 
-			function(resultJson){
-				window.location = action;
-			}, 
-			function(error){
-				console.log("error: "+error.responseText);
-			}
-		);
-		
-		return false;
-	});
+
+	$("#tagAddForm").submit(
+			function() {
+				console.log("submittaa");
+				var request = $(this).serializeObject();
+				request.text = $("#text").tokenInput("get")[0].text
+				request.id = "not yet generated";
+				var action = document.getElementById("tagAddForm")
+						.getAttribute("action");
+				$.postJSON(action, request, function(resultJson) {
+					window.location = action;
+				}, function(error) {
+					console.log("error: " + error.responseText);
+				});
+
+				return false;
+			});
 }
 
-
-
-function initTokenInput(field, model){
+function initTokenInput(field, model) {
 	$(field).tokenInput("/tagcloud/tag/autocomplete", {
 		propertyToSearch : "text",
 		preventDuplicates : true,
 		theme : "facebook",
 		resultsLimit : 8,
-        onAdd: function(){
-            $(model).submit();
-        },
-        onDelete: function(){
-            $(model).submit();
-        }
+		onAdd : function() {
+			$(model).submit();
+		},
+		onDelete : function() {
+			$(model).submit();
+		}
 	});
-	
+
 }
